2916dc6b895f06986522a86cd45350f6b8b672dc
T. Seng <Ozora@MacBook-Pro.lan>
2023-10-02 13:35:47 -0700

    allows user to input max weight

--- pa2.py
+++ pa2.py
@@ -4,7 +4,9 @@ import csv
 with open(sys.argv[1]) as csv_file:
     csv_reader = csv.reader(csv_file)
     arr = []
+    weight = sys.argv[2]
     for line in csv_reader:
         arr.append(line)
     print(arr)
+    print(weight)
         
